d10b84080ac39932338624b3cf75d957cbf0032e
Mitchell Rodríguez <mitrodle@gmail.com>
2021-12-30 16:59:18 -0500

    add: Validar parámetros con class-validator y mapped-types

--- src/dtos/product.dto.ts
+++ src/dtos/product.dto.ts
@@ -1,15 +1,45 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator'; // Decoradores
+
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   readonly price: number;
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   readonly stock: number;
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
+// Validaciones opcionales basadas en CreateProductDto
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  // @IsString()
+  // readonly name?: string;
+  // @IsString()
+  // readonly description?: string;
+  // @IsNumber()
+  // @IsPositive()
+  // readonly price?: number;
+  // @IsNumber()
+  // @IsPositive()
+  // readonly stock?: number;
+  // @IsUrl()
+  // readonly image?: string;
 }
